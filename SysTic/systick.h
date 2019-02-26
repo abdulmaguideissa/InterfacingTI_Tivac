651badc5eaea330d4c323f3d2dca5fea7e1c63a5
Abdulmaguid Eissa <abdulmaguid.eissa@gmail.com>
2019-02-26 23:00:44 +0200

    Add files via upload

---
+++ SysTic/systick.h
@@ -0,0 +1,35 @@
+#ifndef __Delay_H__
+#define __Delay_H__
+/*creating precise time delay using the systick
+ clock registers 
+ *control register.
+ *current register.
+ *reload register.
+ **activating the PLL for 80 MHz is required to get 12.5 ns.
+ */
+
+#define NVIC_ST_CTRL_R      (*((volatile unsigned long *)0xE000E010))
+#define NVIC_ST_RELOAD_R    (*((volatile unsigned long *)0xE000E014))
+#define NVIC_ST_CURRENT_R   (*((volatile unsigned long *)0xE000E018))
+	
+
+void SysTick_Init(void){
+  NVIC_ST_CTRL_R = 0;               // disable SysTick during setup
+  NVIC_ST_CTRL_R = 0x00000005;      // enable SysTick with core clock
+}
+// The delay parameter is in units of the 80 MHz core clock. (12.5 ns)
+void SysTick_Wait(unsigned long delay){
+  NVIC_ST_RELOAD_R = delay-1;  // number of counts to wait
+  NVIC_ST_CURRENT_R = 0;       // any value written to CURRENT clears
+  while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for count flag
+  }
+}
+// 800000*12.5ns equals 10ms
+void SysTick_Wait10ms(unsigned long delay){
+  unsigned long i;
+  for(i=0; i<delay; i++){
+    SysTick_Wait(800000);  // wait 10ms
+  }
+}
+
+#endif
